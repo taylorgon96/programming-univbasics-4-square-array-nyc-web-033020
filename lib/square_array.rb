53f9d6be7acbe0bdb9aab9b27e62aa758c9e5aac
Taylor Gonzalez <taylorgon@gmail.com>
2020-02-28 04:10:03 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,19 +2,10 @@ def square_array(array)
  arr = []
  i = 0
  
-  while i < numbers.size do
+  while i < numbers.size() do
    arr.push(array[i] * array[i])
    i += 1
   end 
   return arr
 end
 
-def square_array(numbers)
-  new_array = []
-  counter = 0 
-  while counter < numbers.length()
-  new_array.push(numbers[counter] * numbers[counter])
-  counter += 1
-  end 
-  return new_array
-end
