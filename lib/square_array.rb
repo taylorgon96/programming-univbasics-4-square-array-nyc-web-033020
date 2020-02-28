6bcd6cb9d9b9b910bf01cd7cfd98949e3a634578
Taylor Gonzalez <taylorgon@gmail.com>
2020-02-28 03:47:05 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,10 @@
 def square_array(array)
  i = 0
  
- while i < array.size
-  puts "#{i} * #{i}"
+ while i < array.size do
+  puts "#{i} ** #{i}"
   i += 1
- end
+ 
   square_array(array)
+end 
 end
