0cc7af70aca96e83961fc5c72420653a0965fb49
Taylor Gonzalez <taylorgon@gmail.com>
2020-02-28 04:09:24 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -8,3 +8,13 @@ def square_array(array)
   end 
   return arr
 end
+
+def square_array(numbers)
+  new_array = []
+  counter = 0 
+  while counter < numbers.length()
+  new_array.push(numbers[counter] * numbers[counter])
+  counter += 1
+  end 
+  return new_array
+end
