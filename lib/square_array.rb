81d4b7d6abea91e37abbb2a7b49517a9defcb4d3
Taylor Gonzalez <taylorgon@gmail.com>
2020-02-28 03:54:39 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,9 +2,9 @@ def square_array(array)
  i = 1
  arr = []
  
-  while i < array.size + 1 do
-   p arr << "#{i} ** #{i}"
+  while i < array.size + 2 do
+   p arr << "#{i} * #{i}"
    i += 1
-   return p arr << "#{i} ** #{i}"
+   return p arr << "#{i} * #{i}"
  end 
 end
